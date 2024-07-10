32844ce076a6b2a598405817fb85ab1f2e8b6671
marlon307 <marlon-ramosb@hotmail.com>
2024-07-09 21:19:39 -0300

    fix Vercel rm cookie parse

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -9,8 +9,8 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private jwt: JwtService) { }
   async use(@Req() req: Request, res: Response, next: NextFunction) {
-    if (req.cookies["authorization"].includes("Bearer ")) {
-      const token = req.cookies["authorization"] || req.headers.authorization;
+    if (req.headers.authorization.includes("Bearer ")) {
+      const token = req.headers.authorization;
       const sucsses = await this.jwt.verifyAsync(token.replace("Bearer ", ""), {
         secret: process.env.SECRETE_KEY,
       })
